053dddc0a7c7d4d1cd91545843857417c0ed324a
jaydeep das <jaydeepBCA005@gmail.com>
2025-01-28 19:42:47 +0530

    1st commit

---
+++ pratice.js
@@ -0,0 +1,8 @@
+// const path = require('fs')
+// path.writeFileSync('text.txt','hyyy','utf-8')
+const str= new Map()
+str.set('name' ,'jay')
+// console.log(str.charAt(1))
+for(let i of str){
+    console.log(i)
+}
